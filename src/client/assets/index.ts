23ba7ed8f4121b80dd5495da73d885f317a62736
jsyang <jsyang.ca@gmail.com>
2018-12-04 00:08:57 +0000

    fully automated static file build: gotta be clever to outsmart parceljs!

--- src/client/assets/index.ts
+++ src/client/assets/index.ts
@@ -1,51 +1,17 @@
 import {defineSound} from './audio';
-import JSZip from 'jszip';
-import JSZipUtils from 'jszip-utils';
+import sounds from './sounds';
 
-const SOUND_FILENAME = /(^sounds\/|\.ogg$)/gi;
-
-// Filter out stuff like .DS_Store
-const FILTER_SOUND_FILENAME = /\.ogg$/i;
-
-interface IZipObject {
-    name: string;
-    async: Function;
-}
-
-function loadAudioFromZip(zip: any): Promise<void> {
-    const soundNames: string[]                        = [];
-    const loadAllSoundBuffers: Promise<ArrayBuffer>[] = [];
-
-    zip.folder('sounds/')
-        .forEach(
-            (relativePath, file: IZipObject) => {
-                if (FILTER_SOUND_FILENAME.test(relativePath)) {
-                    // Get sound name from sound file name
-                    soundNames.push(file.name.replace(SOUND_FILENAME, ''));
-
-                    // Load sound buffers
-                    loadAllSoundBuffers.push(file.async('arraybuffer'));
-                }
-            }
-        );
-
-    return Promise.all(loadAllSoundBuffers)
-        .then(arrayBuffers => arrayBuffers.forEach(
-            (buffer, index) => defineSound({name: soundNames[index], arrayBuffer: buffer})
-        ));
-}
-
-function load(): Promise<void> {
-    return new Promise((resolve, reject) => {
-        // Retrieve remote assets.zip file
-        JSZipUtils.getBinaryContent('assets.zip', (err, data) => {
-            err ? reject(err) : resolve(data);
-        });
-    })
-    // Load zip file into memory
-        .then(data => JSZip.loadAsync(data))
-        // Load audio into memory
-        .then(loadAudioFromZip);
+function load(): Promise<any> {
+    return Promise.all(
+        sounds.map(name =>
+            fetch(`sounds/${name}`)
+                .then(res => res.arrayBuffer())
+                .then(arrayBuffer => defineSound({
+                    name: name.replace('.ogg', ''),
+                    arrayBuffer
+                }))
+        )
+    );
 }
 
 export default {load};
