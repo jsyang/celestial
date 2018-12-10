f90bb63d46dfccd1b4a86e7929a5db20c5f8b434
jsyang <jsyang.ca@gmail.com>
2018-12-10 22:38:12 +0000

    add random flavor text for game over modals

--- src/client/UI/Modal/GameOverModal.ts
+++ src/client/UI/Modal/GameOverModal.ts
@@ -2,6 +2,15 @@ import * as PIXI from 'pixi.js';
 import Modal from '.';
 import Button from '../Button';
 import Score from '../../Score';
+import Random from '../../Random';
+
+const GAME_OVER_MESSAGES = [
+    'Your career ends with a forced\nretirement after recent losses.',
+    'You are dismissed by central command\nfollowing terrible losses.',
+    'The desperation of space warfare has driven you mad.\nYou take a permanent vacation by going AWOL.',
+    'Losing the second major engagement in a row,\nyour subordinates mutiny against you.',
+    'After losing 2 galaxies, your enemies catch you\nand relieve you of duties: by lethal torture.'
+].map(s => s.split('\n'));
 
 function create({onClickContinue}) {
     const modal = Modal.create({width: 400, height: 200});
@@ -22,13 +31,16 @@ function create({onClickContinue}) {
 
     const {rank, score} = Score.getScoreRank();
 
+    const gameOverText = Random.arrayElement(GAME_OVER_MESSAGES);
+
     const label = new PIXI.Text(
         [
             'GAME OVER\n',
-            'Your career ends with a forced',
-            'retirement after recent losses.\n',
-            `Your final rank is ${rank.name}.`,
-            `Your final score is ${score}.`
+            gameOverText[0],
+            gameOverText[1],
+            '',
+            `Your final rank: ${rank.name}.`,
+            `Your final score: ${score}.`
 
         ].join('\n'),
         {
